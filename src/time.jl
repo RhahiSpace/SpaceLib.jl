3332846369f3f20f8bc9250cbb942fa5c88cd3c6
Rhahi <git@rhahi.com>
2023-05-20 01:28:34 +0200

    feat: add pretty print for Timeserver

--- src/time.jl
+++ src/time.jl
@@ -279,3 +279,41 @@ Close the time signalling channel, which results in stopping the update loop.
 Base.close(ts::Timeserver) = close(ts.clients[1])
 next_value!(stream::KRPC.Listener) = KRPC.next_value(stream)
 next_value!(channel::Channel) = take!(channel)
+
+function Base.show(io::IO, ts::Timeserver)
+    status = Base.isopen(ts.clients[1]) ? "active" : "inactive"
+    formatted = ts.type == "MET" ? format_MET(ts.time) : format_UT(ts.time)
+    print(io, "Timeserver ($formatted, $status)")
+end
+
+function decompose_time(t::Int64)
+    S = lpad(t % 60, 2, '0')
+    M = lpad(t ÷ 60 % 60, 2, '0')
+    H = lpad(t ÷ 3600 % 24, 2, '0')
+    D = string(t ÷ 86400)
+    D, H, M, S, ""
+end
+
+function decompose_time(t::Float64)
+    milliseconds = string(round(t % 1, digits=2))[2:end]
+    seconds = convert(Int64, floor(t))
+    D, H, M, S, _ = decompose_time(seconds)
+    D, H, M, S, rpad(milliseconds, 3, "0")
+end
+
+"""Format UT seconds to Day #, ##:##:##[.###] format."""
+function format_UT(t::Real)
+    D, H, M, S, ms = decompose_time(t)
+    hms = join([H, M, S], ':')
+    return string("Day ", D, ", ", hms, ms)
+end
+
+"""Format MET seconds to T+#D ##:##:##[.###] format."""
+function format_MET(t::Real)
+    D, H, M, S, ms = decompose_time(t)
+    hms = join([H, M, S], ':')
+    if D ≠ "0"
+        return string("T+(", D, "d)", hms, ms)
+    end
+    return "T+"*hms*ms
+end
