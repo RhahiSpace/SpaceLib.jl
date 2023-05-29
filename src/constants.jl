890117f3fbd19a397f538ceef2d6e07f757ffd1f
Rhahi <git@rhahi.com>
2023-05-29 23:45:27 +0200

    feat: add indexed constants
    this can be done using spacecenter, but it is a bit too clunky as
    spacecraft does not maintain a conn object.

--- src/constants.jl
+++ src/constants.jl
@@ -9,45 +9,58 @@ const g = 9.80665
 
 # chemistry
 
-const VANILLA_RESOURCES = (
+const VANILLA_DENSITY = Dict{String,Float32}()
 
-)
-
-const RO_RESOURCES = (
-    "Aniline22",
-    "IRFNA-III",
-    "Nitrogen",
-    "Helium",
-    "ElectricCharge",
-)
-
-const DENSITY = Dict{String,Float32}(
-    "Aniline22" => 1.23,
+const RO_DENSITY = Dict{String,Float32}(
+    "AK20" => 1.499,
+    "AK27" => 1.494,
+    "Ammonia" => 0.000769,
+    "Aniline" => 1.02,
+    "Aniline22" => 1.042,
+    "Aniline37" => 1.0585,
+    "ElectricCharge" => 0.0,
+    "Helium" => 0.0001786,
+    "HTP" => 1.431,
+    "Hydrazine" => 1.004,
+    "Hydrogen" => 8.99e-5,
     "IRFNA-III" => 1.658,
+    "Kerosene" => 0.82,
+    "LqdAmmonia" => 0.7021,
+    "LqdOxygen" => 1.1409999,
     "Nitrogen" => 0.001251,
+    "SoundingPayload" => 0.5,
+    "Water" => 1.0,
 )
 
-function populate_density!(table::Dict{String,Float32}, sc::SpaceCenter;
-    resources=RO_RESOURCES,
+function populate_density!(sc::SpaceCenter;
+    table::Dict{String,Float32}=RO_DENSITY,
     extras=nothing
 )
-    for name ∈ resources
-        table[name] = SCH.Density(sc.conn, name)
+    for name ∈ table
+        new = SCH.Density(sc.conn, name)
+        old = table[name]
+        if new ≉ old
+            @warn "Detected changed value for $name ($old -> $new)"
+            table[name] = new
+        end
     end
     if !isnothing(extras)
         for name ∈ extras
-            if name ∉ resources
-                d = SCH.Density(sc.conn, name)
-                table[name] = d
-                @warn "New substance found, consider adding density for $name => $d" _group=:system
+            if name ∉ table
+                value = SCH.Density(sc.conn, name)
+                table[name] = value
+                entry = "\"$name\" => $value"
+                @warn "New resource found, consider adding to density table" entry _group=:system
             end
         end
     end
 end
 
-function density(name::String)
-    value = get(DENSITY, name, nothing)
-    isnothing(value) && error("$name is not a registered resource")
+"kg per in-game unit"
+function density(name::String; table=RO_DENSITY)
+    density = get(table, name, nothing)
+    isnothing(density) && error("$name is not a registered resource")
+    return density
 end
 
 end # module
