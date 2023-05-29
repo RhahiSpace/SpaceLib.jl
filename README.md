a079567b96000435458af82f356a758e0a7bed06
Rhahi <git@rhahi.com>
2023-05-29 23:51:43 +0200

    doc: apply correct information of documentation

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # SpaceLib
 
-[![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://rhahi.github.io/SpaceLib.jl/stable/)
-[![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://rhahi.github.io/SpaceLib.jl/dev/)
-[![Build Status](https://github.com/rhahi/SpaceLib.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/rhahi/SpaceLib.jl/actions/workflows/CI.yml?query=branch%3Amain)
+[![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://docs.rhahi.space/SpaceLib.jl/stable/)
+[![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://docs.rhahi.space/SpaceLib.jl/dev/)
--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,20 @@
 using SpaceLib
 using Documenter
 
-DocMeta.setdocmeta!(SpaceLib, :DocTestSetup, :(using SpaceLib); recursive=true)
+DocMeta.setdocmeta!(SpaceLib, :DocTestSetup, :(using SpaceLib); recursive = true)
 
 makedocs(;
-    modules=[SpaceLib],
-    authors="Rhahi <git@rhahi.com> and contributors",
-    repo="https://github.com/rhahi/SpaceLib.jl/blob/{commit}{path}#{line}",
-    sitename="SpaceLib.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://rhahi.github.io/SpaceLib.jl",
-        edit_link="main",
-        assets=String[],
+    modules = [SpaceLib],
+    authors = "Rhahi <git@rhahi.com>",
+    repo = "https://github.com/RhahiSpace/SpaceLib.jl/blob/{commit}{path}#{line}",
+    sitename = "SpaceLib.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://docs.rhahi.space/SpaceLib.jl",
+        edit_link = "main",
+        assets = String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = ["Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/rhahi/SpaceLib.jl",
-    devbranch="main",
-)
+deploydocs(; repo = "github.com/RhahiSpace/SpaceLib.jl", devbranch = "main")
